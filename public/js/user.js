25c587f3a8b6b557e00849e6425338139cc0ea08
BAE BYEONGIL <b000202@naver.com>
2023-06-30 01:01:56 +0800

    1

--- public/js/user.js
+++ public/js/user.js
@@ -1,22 +1,21 @@
-
 const form = document.getElementById("signup");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const payload = new FormData(form);
+  const payload = new FormData(form);
 
-    console.log([...payload]);
+  console.log([...payload]);
 
-    fetch("http://localhost:8000/api/users", {
-        method : "POST",
-        headers : {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body : payload,
-    })
-    .then(res => res.json())
-    .then(data => console.log(data));
+  fetch("http://localhost:8000/api/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: payload,
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 });
 
 // user.html 에서 받은 id password passwordconfirm 정보를 FormData에 담아 POST 하려 했으나 실패함
