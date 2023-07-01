89831a6a8cd8d035cc872dc8502d0f4da6fbe04f
hyem00 <hemi1738@naver.com>
2023-07-02 05:02:15 +0900

    에러 장난하냐 지굼

--- public/js/update.js
+++ public/js/update.js
@@ -1,32 +1,32 @@
 function update(post_id) {
-  const form = document.getElementById("post-form");
+  console.log(post_id);
+  // const form = document.getElementById("post-form");
 
-  form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const payload = new FormData(form);
+  // form.addEventListener("submit", async (e) => {
+  //   e.preventDefault();
+  //   const payload = new FormData(form);
 
-    const myForm = {};
-    await payload.forEach((value, key) => (myForm[key] = value));
+  //   const myForm = {};
+  //   await payload.forEach((value, key) => (myForm[key] = value));
 
-    fetch("http://localhost:8000/api/posts/" + post_id, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(myForm),
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res);
-        if (res.errorMessage === "해당 게시글을 찾을 수 없습니다.") {
-          alert("해당 게시글을 찾을 수 없습니다.");
-        } else if (res.message === "권한이 없습니다.") {
-          alert("권한이 없습니다.");
-        } else {
-          alert("수정완료.");
-          location.href = "main";
-        }
-      });
-  });
+  //   fetch("http://localhost:8000/api/posts/" + post_id, {
+  //     method: "PUT",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(myForm),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((res) => {
+  //       console.log(res);
+  //       if (res.errorMessage === "해당 게시글을 찾을 수 없습니다.") {
+  //         alert("해당 게시글을 찾을 수 없습니다.");
+  //       } else if (res.message === "권한이 없습니다.") {
+  //         alert("권한이 없습니다.");
+  //       } else {
+  //         alert("수정완료.");
+  //         location.href = "main";
+  //       }
+  //     });
+  // });
 }
-//
