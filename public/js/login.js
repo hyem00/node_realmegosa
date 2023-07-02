0921ee1efe0a19743c45dd68925d76346ef2c6b1
Jang Sihun <chocoj1123@naver.com>
2023-07-02 20:09:52 +0900

    2

--- public/js/login.js
+++ public/js/login.js
@@ -26,32 +26,37 @@ loginForm.addEventListener("submit", (e) => {
         localStorage.setItem("token", res.token);
         location.reload();
       }
-      // 로그인 상태 확인 함수
-      function isLoggedIn() {
-        const token = localStorage.getItem("token");
-        // 토큰의 존재 여부로 로그인 상태를 확인할 수 있습니다.
-        return token !== null;
-      }
 
-      // 로그인/로그아웃 버튼 텍스트 변경 함수
-      function updateLoginButton() {
-        const loginButton = document.querySelector("#login-btn");
-        if (isLoggedIn()) {
-          loginButton.textContent = "로그아웃";
-        } else {
-          loginButton.textContent = "로그인";
-        }
-      }
+      // // 로그인 상태 확인 함수
+      // function isLoggedIn() {
+      //   const token = localStorage.getItem("token");
+      //   const kakologin = localStorage.getItem(
+      //     "kakao_eacc25aabf6087a59d0785e34de0a93b"
+      //   );
+      //   // 토큰의 존재 여부로 로그인 상태를 확인할 수 있습니다.
+      //   return token !== null;
+      //   return kakao_eacc25aabf6087a59d0785e34de0a93b !== null;
+      // }
 
-      function hideJoinButton() {
-        const joinButton = document.querySelector("#joinBtn");
-        if (isLoggedIn()) {
-          joinButton.textContent = "마이페이지";
-          joinButton.addEventListener("click", redirectToMyPage);
-        } else {
-          joinButton.textContent = "회원가입";
-        }
-      }
+      // // 로그인/로그아웃 버튼 텍스트 변경 함수
+      // function updateLoginButton() {
+      //   const loginButton = document.querySelector("#login-btn");
+      //   if (isLoggedIn()) {
+      //     loginButton.textContent = "로그아웃";
+      //   } else {
+      //     loginButton.textContent = "로그인";
+      //   }
+      // }
+
+      // function hideJoinButton() {
+      //   const joinButton = document.querySelector("#joinBtn");
+      //   if (isLoggedIn()) {
+      //     joinButton.textContent = "마이페이지";
+      //     joinButton.addEventListener("click", redirectToMyPage);
+      //   } else {
+      //     joinButton.textContent = "회원가입";
+      //   }
+      // }
 
       function redirectToMyPage() {
         const myPageURL = "/mypage";
@@ -86,28 +91,61 @@ loginForm.addEventListener("submit", (e) => {
     });
 });
 
-Kakao.init("683d473caa873ed84400194b1b27aab0");
-document.getElementById("kakao-login-btn").addEventListener("click", () => {
-  // 카카오 로그인 실행
-  Kakao.Auth.loginForm({
-    scope: "profile_nickname, account_email, gender, profile_image",
-    success: function (authObj) {
-      console.log(authObj);
-      Kakao.API.request({
-        url: "/v2/user/me",
-        success: (res) => {
-          const kakao_account = res.kakao_account;
-          console.log(kakao_account);
-          hideModal();
-        },
-      });
-    },
-  });
+// Kakao.init("683d473caa873ed84400194b1b27aab0");
+// document.getElementById().addEventListener("click", () => {
+//   // 카카오 로그인 실행
+//   Kakao.Auth.loginForm({
+//     scope: "profile_nickname, account_email, gender, profile_image",
+//     success: function (authObj) {
+//       console.log(authObj);
+//       Kakao.API.request({
+//         url: "/v2/user/me",
+//         success: (res) => {
+//           const kakao_account = res.kakao_account;
+//           console.log(kakao_account);
+//           hideModal();
+//         },
+//       });
+//     },
+//   });
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  logincheck();
 });
 
-function KakaoLogout() {
-  Kakao.Auth.logout(function () {
-    console.log("Kakao 로그아웃 완료");
-    // 로그아웃 후에 수행할 작업을 추가하면 됩니다.
-  });
+function logincheck() {
+  const checkToken = document.cookie.split("=")[1];
+  const topBar = document.querySelector(".signBox");
+  console.log(checkToken);
+  let temp = ``;
+  if (checkToken) {
+    temp = `<li><a href="#"><img src="../img/pngwing.com.png" onclick="post()"></a></li>
+            <button onclick="mypage()">마이페이지</button>
+            <button onclick="logout()">로그아웃</button>
+          `;
+  } else {
+    temp = `<button onclick="join()">회원가입</button>
+            <button onclick="showModal()">로그인</button>
+          `;
+  }
+  topBar.innerHTML += temp;
+}
+
+function mypage() {
+  location.href = "mypage";
+}
+
+function logout() {
+  fetch("http://localhost:8000/api/logout", {
+    method: "GET",
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      alert("로그아웃 되었습니다.");
+      localStorage.removeItem("token");
+      localStorage.removeItem("kakao_eacc25aabf6087a59d0785e34de0a93b");
+      location.reload();
+    });
 }
